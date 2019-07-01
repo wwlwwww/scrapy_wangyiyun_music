af6536141c74e528fb4686aa4493e1462c6fe104
wmlhust <wmlhust@qq.com>
2019-07-01 23:13:11 +0800

    update setting

--- music163/settings.py
+++ music163/settings.py
@@ -21,7 +21,7 @@ NEWSPIDER_MODULE = 'music163.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 4096
+CONCURRENT_REQUESTS = 8192
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
@@ -36,10 +36,11 @@ RETRY_ENABLED = True
 RETRY_TIMES = 2
 RETRY_PRIORITY_ADJUST = 1
 
-
 # The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 30
-# CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 1024
+CONCURRENT_REQUESTS_PER_IP = 1024
+
+
 
 # Disable cookies (enabled by default)
 # COOKIES_ENABLED = False
@@ -84,12 +85,14 @@ ITEM_PIPELINES = {
     'music163.pipelines.my_pipeline': 300,
 }
 
+CONCURRENT_ITEMS = 1000
+
 # DB_PATH = 'C:/SQLite/DB/'
 DB_PATH = "/home/wml/db/tp_db/"
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-# AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = False
 # The initial download delay
 # AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
@@ -107,3 +110,5 @@ DB_PATH = "/home/wml/db/tp_db/"
 # HTTPCACHE_DIR = 'httpcache'
 # HTTPCACHE_IGNORE_HTTP_CODES = []
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+MEMUSAGE_ENABLED = False
