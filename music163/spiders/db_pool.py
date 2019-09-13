99bed2cd973f01850df29af113ec3a1d5d84f666
wmlhust <wmlhust@qq.com>
2019-09-13 21:35:27 +0800

    db update to mysql

--- music163/spiders/db_pool.py
+++ music163/spiders/db_pool.py
@@ -5,4 +5,4 @@ import DBUtils
 import pymysql
 
 # db = pymysql.connect(host="172.27.0.10", port=3306, user="root", password="wmlhust12", db="music163")
-pool = DBUtils.PersistentDB(creator=pymysql, maxusage=100, host="172.27.0.10", port=3306, user="root", password="wmlhust12", database="music163", charset='utf8')
+pool = DBUtils.PersistentDB.PersistentDB(creator=pymysql, maxusage=100, host="172.27.0.10", port=3306, user="root", password="wmlhust12", database="music163", charset='utf8')
