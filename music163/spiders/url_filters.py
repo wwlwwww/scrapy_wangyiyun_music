c83cb068c303f6cd9a16c56bb37794c519fdb908
wmlhust <wmlhust@qq.com>
2019-09-14 10:46:37 +0800

    db update to mysql

--- music163/spiders/url_filters.py
+++ music163/spiders/url_filters.py
@@ -4,25 +4,26 @@ import os
 from os import path
 from urllib.parse import parse_qs, urlparse
 
+import pymysql
 from bitmap.bitmap import BitMap
 from scrapy.dupefilters import RFPDupeFilter, BaseDupeFilter
 
 from music163.spiders import db_pool
 
-
 class artist_id_fileter(BaseDupeFilter):
     def __init__(self):
+        db = pymysql.connect(host="172.27.0.10", port=3306, user="root", password="wmlhust12", db="music163")
+
         self.seen_artist = BitMap(20000000)
-        conn = db_pool.pool.connection()
-        cursor = conn.cursor()
+        cursor = db.cursor()
 
         cursor.execute('select f_id, f_music_size from t_artists')
-        for row in cursor.fetchone():
+        for row in cursor:
             if int(row[0]) > 0 or int(row[0]) < 0:
                 self.seen_artist.set(row[0])
 
         cursor.close()
-        conn.close()
+        db.close()
 
         logging.info("seenid, cnt: {}".format(self.seen_artist.count()))
 
